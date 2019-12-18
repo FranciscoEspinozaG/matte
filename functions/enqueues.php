b8753a74562dd7f087ac292e4b22634f692c17f4
FranciscoEspinozaG <FranciscoEspinozaG@users.noreply.github.com>
2019-12-18 15:58:36 -0300

    mismo error de carga eterna

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -34,6 +34,8 @@ if ( ! function_exists('b4st_enqueues') ) {
 		wp_register_style('fontawesome5', 'https://use.fontawesome.com/releases/v5.11.1/css/all.css', false, '5.11.1', null);
 		wp_enqueue_style('fontawesome5');
 
+		wp_enqueue_style( 'gutenberg-blocks', get_template_directory_uri() . '/assets/css/blocks.css' );
+
 		wp_register_style('owl-carousel-css', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css', true, '2.3.4');
 		wp_enqueue_style('owl-carousel-css');
 
@@ -52,6 +54,25 @@ if ( ! function_exists('b4st_enqueues') ) {
 
 		// Scripts
 
+		
+		wp_register_script('modernizr',  'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', false, '2.8.3', true);
+		wp_enqueue_script('modernizr');
+		
+		wp_enqueue_script('jquery');
+		
+		wp_register_script('bootstrap-bundle', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js', false, '4.1.3', true);
+		wp_enqueue_script('bootstrap-bundle');
+		// (The Bootstrap JS bundle contains Popper JS.)
+		
+		wp_register_script('owl-carousel-js', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js', true, '2.3.4');
+		wp_enqueue_script('owl-carousel-js');
+		
+		wp_register_script('masonry',  get_template_directory_uri() .'/assets/js/masonry.js', false, '4.2.2', false);
+		wp_enqueue_script('masonry');
+
+		wp_register_script('b4st', get_template_directory_uri() . '/assets/js/b4st.js', false, null, true);
+		wp_enqueue_script('b4st');
+		
 		wp_register_script('main', get_template_directory_uri() . '/assets/js/main.js', false, null, false);
 		$urlTema = get_template_directory_uri();
 		$script_data = array(
@@ -63,23 +84,8 @@ if ( ! function_exists('b4st_enqueues') ) {
 			'my_data',
 			$script_data
 		);
+		
 		wp_enqueue_script('main');
-		
-		wp_register_script('masonry',  get_template_directory_uri() .'/assets/js/masonry.js', false, '4.2.2', false);
-		wp_enqueue_script('masonry');
-
-		wp_register_script('modernizr',  'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js', false, '2.8.3', true);
-		wp_enqueue_script('modernizr');
-		
-		wp_enqueue_script('jquery');
-
-		wp_register_script('bootstrap-bundle', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js', false, '4.1.3', true);
-		wp_enqueue_script('bootstrap-bundle');
-		// (The Bootstrap JS bundle contains Popper JS.)
-
-		wp_register_script('owl-carousel-js', 'https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js', true, '2.3.4');
-		wp_enqueue_script('owl-carousel-js');
-
 		if (is_singular() && comments_open() && get_option('thread_comments')) {
 			wp_enqueue_script('comment-reply');
 		}
